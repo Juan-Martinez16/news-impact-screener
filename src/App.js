53933701ec153db50df46d6894ade82f22e56c9b
Juan Martinez <jmmg93@hotmail.com>
2025-08-03 20:45:49 +0100

    Fix App.js to load NewsImpactScreener component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-// src/App.js - DEFINITIVE VERSION
-// This fixes the critical import issue in your current App.js
-
 import React from "react";
 import "./App.css";
 import NewsImpactScreener from "./components/NewsImpactScreener";
-import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
-    <ErrorBoundary>
-      <div className="App">
-        <NewsImpactScreener />
-      </div>
-    </ErrorBoundary>
+    <div className="App">
+      <NewsImpactScreener />
+    </div>
   );
 }
 
